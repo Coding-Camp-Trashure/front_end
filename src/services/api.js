edbeeb4434e760d0c3cfceeb40938b3641f319bf
vozert <novalriski112@gmail.com>
2025-06-12 13:03:20 +0700

    Integration 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
+});
+
+// Remove /api from URL since it's already in the backend URL structure
+api.interceptors.request.use((config) => {
+  // Remove any double slashes in the URL except after http(s):
+  config.url = config.url.replace(/([^:]\/)\/+/g, "$1");
+  return config;
 });
 
 // Add response interceptor for error handling
